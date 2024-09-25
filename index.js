97ac20e26243001256ad83f1ec5ebea98dae31c4
CeutaRossmary <ceutarossmary@gmail.com>
2024-09-25 17:52:03 -0300

    diseño

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector(".btn-dark").addEventListener("click", () => {
+  const propina = document.querySelector('input[aria-label="Propina"]').value;
+  alert(`Propina ingresada: ${propina}`);
+});
