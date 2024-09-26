6ab050aa17006fe0a43fbf36941a2dff0c167d8a
CeutaRossmary <ceutarossmary@gmail.com>
2024-09-26 16:34:03 -0300

    calcular propinas y ingredientes adicionales

--- index.js
+++ index.js
@@ -1,4 +1,42 @@
-document.querySelector(".btn-dark").addEventListener("click", () => {
-  const propina = document.querySelector('input[aria-label="Propina"]').value;
-  alert(`Propina ingresada: ${propina}`);
+const checks = document.querySelectorAll('input[type="checkbox"]');
+const seleccion = document.getElementById("seleccion");
+const adicionales = document.getElementById("adicionales");
+const extras = document.getElementById("extras");
+const spanPropiana = document.getElementById("spanPropina");
+const btn = document.getElementById("btn");
+
+ var sumar = 0;
+checks.forEach((box) => {
+  box.addEventListener("change", () => {
+    extras.innerHTML = "";
+    seleccion.innerHTML = "";
+    adicionales.innerHTML = "";
+    let count = 0;
+    checks.forEach((e) => {
+      if (e.checked) {
+        count++;
+        if (count <= 3) {
+          seleccion.innerHTML += `<li>${e.value} , </li>`;
+        } else {
+          adicionales.innerHTML += `<li>${e.value} , </li>`;
+          sumar += 800
+          extras.innerHTML = `$${sumar}`;
+          console.log(sumar);
+        }
+      }
+    });
+  });
+});
+
+spanPropiana.innerHTML = "$1.000";
+btn.addEventListener("click", () => {
+  const propina = document.getElementById("propina").value;
+
+  if (propina <= 0 || propina <= 1000) {
+    alert("Su propina de propina: $1.000 ha sido enviada.");
+    spanPropiana.innerHTML = "$1.000";
+  } else if (propina > 1000) {
+    spanPropiana.innerHTML = `$${propina}`;
+    alert(`Su propina de propina: $${propina} ha sido enviada.`);
+  }
 });
